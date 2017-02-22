d9fa4009abbba0af20870a1df212770e6539af63
bdeining <benjamin.deininger@connexta.com>
2017-02-21 20:32:51 -0700

    Finished Client

--- assignment2/src/main/java/cs455/scaling/client/Client.java
+++ assignment2/src/main/java/cs455/scaling/client/Client.java
@@ -1,19 +1,17 @@
 package cs455.scaling.client;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Client {
 
@@ -23,9 +21,15 @@ public class Client {
 
     private static int serverPort;
 
+    private Lock lock = new ReentrantLock();
+
     private Selector selector;
 
     private static int messageRate;
+
+    private WriteThread writeThread;
+
+    private long messagesReceived;
 
     public static void main(String[] args) {
         if (args.length != 3) {
@@ -54,68 +58,68 @@ public class Client {
     }
 
     private void startClient() throws IOException, InterruptedException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+
         selector = Selector.open();
         SocketChannel channel = SocketChannel.open();
         channel.configureBlocking(false);
         channel.connect(new InetSocketAddress(serverHost, serverPort));
         channel.register(selector, SelectionKey.OP_CONNECT);
 
+        long before = System.currentTimeMillis() / 1000;
+
         while (true) {
             selector.select();
 
             Iterator i = selector.selectedKeys().iterator();
-            System.out.println(selector.selectedKeys().size());
+
             while (i.hasNext()) {
                 SelectionKey key = (SelectionKey) i.next();
 
                 i.remove();
                 if (key.isConnectable()) {
-                    System.out.println("connectible");
                     if (channel.finishConnect()) {
                         key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                        writeThread = new WriteThread(channel, messageRate, lock, hashCodes);
+                        new Thread(writeThread).start();
                     }
                 } else if (key.isReadable()) {
-                    System.out.println("read");
                     ByteBuffer buf = ByteBuffer.allocate(8000);
                     buf.rewind();
                     int read = channel.read(buf);
-                    System.out.println("bytes read " + read);
                     byte[] data = new byte[read];
                     System.arraycopy(buf.array(), 0, data, 0, read);
-                    System.out.println(new String(data));
+                    String hashCode = new String(data);
+                    removeHashCode(hashCode);
+                    messagesReceived++;
                 } else if (key.isWritable()) {
-                    System.out.println("write");
-                    ByteBuffer buffer = generateMessage();
-                    channel.write(buffer);
-                    buffer.clear();
-                    Thread.sleep(1000 / messageRate);
+                    writeThread.setKey(key);
                 }
+            }
+
+            long after = System.currentTimeMillis() / 1000;
+            if(after - before == 10) {
+                Calendar cal = Calendar.getInstance();
+                String output = String.format("[%s] Total Sent Count: %s, Total Received Count: %s", sdf.format(cal.getTime()), writeThread.getMessagesSent(), messagesReceived);
+                System.out.println(output);
+                before = System.currentTimeMillis() / 1000;
             }
         }
     }
 
-    private ByteBuffer generateMessage() {
-        byte[] bytes = new byte[8000];
-        for (int i = 0; i < 8000; i++) {
-            bytes[i] = (byte) i;
-        }
+    private void removeHashCode(String hashCode) {
         try {
-            String hashCode = SHA1FromBytes(bytes);
-            System.out.println("sending " + hashCode);
-            hashCodes.add(hashCode);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            lock.lock();
+            for (String comp : hashCodes) {
+                if (hashCode.equals(comp)) {
+                    System.out.println("remove " + hashCode);
+                    hashCodes.remove(hashCode);
+                }
+            }
+        } finally {
+            lock.unlock();
         }
-
-        return ByteBuffer.wrap(bytes);
-    }
-
-    //TODO Common Util
-    private String SHA1FromBytes(byte[] data) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA1");
-        byte[] hash = digest.digest(data);
-        BigInteger hashInt = new BigInteger(1, hash);
-
-        return hashInt.toString(16);
     }
 }
---
+++ assignment2/src/main/java/cs455/scaling/client/WriteThread.java
@@ -0,0 +1,100 @@
+package cs455.scaling.client;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+
+public class WriteThread implements Runnable {
+
+    private SelectionKey selectionKey;
+
+    private SocketChannel socketChannel;
+
+    private Lock lock;
+
+    private List<String> hashCodes;
+
+    private int messageRate;
+
+    private long messagesSent;
+
+    public WriteThread(SocketChannel socketChannel, int messageRate, Lock lock, List<String> hashCodes) {
+        this.messageRate = messageRate;
+        this.socketChannel = socketChannel;
+        this.lock = lock;
+        this.hashCodes = hashCodes;
+    }
+
+    public void setKey(SelectionKey selectionKey) {
+        this.selectionKey = selectionKey;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+
+            if (selectionKey != null && selectionKey.isWritable()) {
+                System.out.println("write message");
+                ByteBuffer buffer = generateMessage();
+                try {
+                    socketChannel.write(buffer);
+                    messagesSent++;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                buffer.clear();
+                selectionKey = null;
+            }
+
+            try {
+                Thread.sleep(1000 / messageRate);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private ByteBuffer generateMessage() {
+        byte[] bytes = new byte[8000];
+        for (int i = 0; i < 8000; i++) {
+            bytes[i] = (byte) getRandomInt();
+        }
+        try {
+            String hashCode = SHA1FromBytes(bytes);
+            System.out.println("sending " + hashCode);
+            lock.lock();
+            hashCodes.add(hashCode);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+        return ByteBuffer.wrap(bytes);
+    }
+
+    //TODO Common Util
+    private String SHA1FromBytes(byte[] data) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA1");
+        byte[] hash = digest.digest(data);
+        BigInteger hashInt = new BigInteger(1, hash);
+
+        return hashInt.toString(16);
+    }
+
+    private int getRandomInt() {
+        Random random = new Random();
+        return random.nextInt();
+    }
+
+    public long getMessagesSent() {
+        return messagesSent;
+    }
+}
